c9fb5d4aa20260cb19afe724052e51ddb274bd59
koyashiro <6698252+koyashiro@users.noreply.github.com>
2024-04-20 16:41:53 +0900

    feat: support stdin

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,26 @@ mod args;
 mod error;
 mod pitch;
 
-use crate::{args::Args, pitch::PitchWithFormat};
+use std::io::{stdin, Read};
 
 use clap::Parser;
-use pitch::PitchFormat;
+
+use crate::{
+    args::Args,
+    pitch::{PitchFormat, PitchWithFormat},
+};
 
 fn main() {
     let args = Args::parse();
 
-    let pitch_with_format: PitchWithFormat = args.pitch.parse().unwrap();
+    let pitch = args.pitch.unwrap_or_else(|| {
+        let mut buf = String::new();
+        stdin().lock().read_to_string(&mut buf).unwrap();
+        buf.truncate(buf.trim_end().len());
+        buf
+    });
+
+    let pitch_with_format: PitchWithFormat = pitch.parse().unwrap();
 
     match pitch_with_format.format {
         PitchFormat::ScientificPitchNotation => {
